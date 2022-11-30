2e23c74037a462b6408d1ce67234a0c6aa2dc183
Ahmed Abid <92954851+AhmedAb1d@users.noreply.github.com>
2022-11-30 14:50:08 +0100

    Set up the application

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4200;
+const port = 3200;
 
 app.get('/', (req, res) => {
   res.send('Hello world');
--- frontend/src/Pages/App.js
+++ frontend/src/Pages/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from '../Utils/logo.png';
 
 function App() {
   return (
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Inceptum
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docs.google.com/presentation/d/1BbD8PRGJpBazYToJ7sNPb8br2Eupwcol/edit?usp=sharing&ouid=106270868181880049023&rtpof=true&sd=true"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to the presentation
         </a>
       </header>
     </div>
